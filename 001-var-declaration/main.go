9f7e6ea98fbbc54ce691cf67e75f208fcfd40d67
Isidro Hortelano <isidrohortelano@gmail.com>
2021-01-11 16:24:15 +0100

    test short declaration

--- 001-var-declaration/main.go
+++ 001-var-declaration/main.go
@@ -4,10 +4,15 @@ import "fmt"
 
 var y = 44
 
+// Assign Zero by default for Type int to "z"
+var z int
+
 func main() {
 	//Short declaration
 	x := 43
 	fmt.Println(x)
 
 	fmt.Println(y)
+
+	fmt.Println(z)
 }
